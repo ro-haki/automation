c8f027e248c5d7ef8034fb6eb1db2b8e59ead2a1
ro-haki <rohakimy@gmail.com>
2025-05-25 21:51:46 +0300

    ec2 setup for ubuntu and debian

---
+++ ec2_setup_ubuntu.sh
@@ -0,0 +1,34 @@
+sudo apt update
+
+# install usefull tools
+sudo apt install git tree net-tools tmux htop -y
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# set up docker compose
+sudo apt install docker-compose-plugin -y
+
+# set up user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+echo "Relogin to be able to run docker without sudo"
+
+# create .env
+cp env.example .env
+
+# build containers
+docker compose build
